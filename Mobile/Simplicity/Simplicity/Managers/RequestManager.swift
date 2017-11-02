22cbfc338a5f9d0ebafb49304008a4528f81ec43
Anthony Lai <yukuan.anthony.lai@gmail.com>
2017-11-02 14:38:13 -0700

    Add Podfile and install Cocoapods

---
+++ Mobile/Simplicity/Simplicity/Managers/RequestManager.swift
@@ -0,0 +1,13 @@
+//
+//  RequestManager.swift
+//  Simplicity
+//
+//  Created by Anthony Lai on 11/2/17.
+//  Copyright © 2017 LikeX4Y. All rights reserved.
+//
+
+import SwiftyJSON
+
+class RequestManager: NSObject {
+
+}
