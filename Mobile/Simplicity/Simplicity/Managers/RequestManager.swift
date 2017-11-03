773b2a49246a6f63ed40e059b0ffedd4960384ba
Anthony Lai <yukuan.anthony.lai@gmail.com>
2017-11-02 17:02:12 -0700

    Add basic RequestManager and use AddViewController to test JSON response

--- Mobile/Simplicity/Simplicity/Managers/RequestManager.swift
+++ Mobile/Simplicity/Simplicity/Managers/RequestManager.swift
@@ -9,5 +9,25 @@
 import SwiftyJSON
 
 class RequestManager: NSObject {
-
+    
+    let baseURL = "https://jsonplaceholder.typicode.com"
+    static let sharedInstance = RequestManager()    // singleton
+    static let getPostsEndpoint = "/posts/"
+    
+    func getPostWithId(postId: Int, onSuccess: @escaping(JSON) -> Void, onFailure: @escaping(Error) -> Void){
+        let url: String = baseURL + RequestManager.getPostsEndpoint + String(postId)
+        let request: NSMutableURLRequest = NSMutableURLRequest(url: NSURL(string: url)! as URL)
+        request.httpMethod = "GET"
+        
+        let session = URLSession.shared
+        let task = session.dataTask(with: request as URLRequest, completionHandler: {data, response, error -> Void in
+            if(error != nil){
+                onFailure(error!)
+            } else{
+                let result = JSON(data: data!)
+                onSuccess(result)
+            }
+        })
+        task.resume()
+    }
 }
