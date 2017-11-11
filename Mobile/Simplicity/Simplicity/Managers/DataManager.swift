13681fe4f5632d689162984fa13cc17a022acc81
Anthony Lai <yukuan.anthony.lai@gmail.com>
2017-11-10 19:01:37 -0800

    Add Models and DataManager

---
+++ Mobile/Simplicity/Simplicity/Managers/DataManager.swift
@@ -0,0 +1,32 @@
+//
+//  DataManager.swift
+//  Simplicity
+//
+//  Created by Anthony Lai on 11/10/17.
+//  Copyright © 2017 LikeX4Y. All rights reserved.
+//
+
+import UIKit
+
+class DataManager: NSObject {
+    
+    static let sharedInstance = DataManager()
+    
+    private var _transactions: [Transaction]?
+    
+    private override init() {
+        super.init()
+    }
+    
+    func getTransactions() -> [Transaction] {
+        if (_transactions == nil) {
+            // TODO: get transactions from RequestManager
+            _transactions = []
+            _transactions?.append(Transaction(transactionId: 1, userId: 1, category: "test", amount: 5.00, date: NSDate())!)
+            _transactions?.append(Transaction(transactionId: 2, userId: 1, category: "test", amount: 9.00, date: NSDate())!)
+        }
+        
+        return _transactions!
+    }
+    
+}
---
+++ Mobile/Simplicity/Simplicity/Models/Transaction.swift
@@ -0,0 +1,35 @@
+//
+//  Transaction.swift
+//  Simplicity
+//
+//  Created by Anthony Lai on 11/10/17.
+//  Copyright © 2017 LikeX4Y. All rights reserved.
+//
+
+import Foundation
+
+class Transaction {
+    
+    // MARK: Properties
+    
+    var trancationId: Int
+    var userId: Int
+    var category: String
+    var amount: Double
+    var date: NSDate
+    
+    
+    // MARK: Initialization
+    init?(transactionId: Int, userId: Int, category: String, amount: Double, date: NSDate) {
+        
+        if (transactionId < 1 || userId < 1 || amount < 0.0) {
+            return nil
+        }
+        
+        self.trancationId = transactionId
+        self.userId = userId
+        self.category = category
+        self.amount = amount
+        self.date = date
+    }
+}
