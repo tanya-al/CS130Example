6e5132119221083c5aeefe90c15e86caf703acf1
Anthony Lai <yukuan.anthony.lai@gmail.com>
2017-11-10 22:24:07 -0800

    Add dummy pie chart

--- Mobile/Simplicity/Simplicity/Controllers/OverviewViewController.swift
+++ Mobile/Simplicity/Simplicity/Controllers/OverviewViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Charts
 
 class OverviewViewController: UIViewController {
     
@@ -19,6 +20,8 @@ class OverviewViewController: UIViewController {
         
         let mainNavigationBar = MainNavigationBar(frame: view.frame, title: "Overview")
         view.addSubview(mainNavigationBar)
+        
+        setOverviewPieChart()
     }
     
     override func didReceiveMemoryWarning() {
@@ -26,5 +29,39 @@ class OverviewViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func setOverviewPieChart() {
+        
+        let pieChartView = PieChartView(frame: self.view.frame)
+        let overviews = DataManager.sharedInstance.getOverviews()
+        var dataEntries: [PieChartDataEntry] = []
+        
+        for i in 0..<overviews.count {
+            let dataEntry = PieChartDataEntry(value: overviews[i].percentage, label: overviews[i].category)
+            dataEntries.append(dataEntry)
+        }
+        
+        let pieChartDataSet = PieChartDataSet(values: dataEntries, label: "Overview")
+        
+        var colors: [UIColor] = []
+        for _ in 0..<overviews.count {
+            let red = Double(arc4random_uniform(256))
+            let green = Double(arc4random_uniform(256))
+            let blue = Double(arc4random_uniform(256))
+            let color = UIColor(red: CGFloat(red/255), green: CGFloat(green/255), blue: CGFloat(blue/255), alpha: 1)
+            colors.append(color)
+        }
+        
+        pieChartDataSet.colors = colors
+        pieChartView.data = PieChartData(dataSet: pieChartDataSet)
+        pieChartView.noDataText = "No data available"
+        
+        let d = Description()
+        d.text = "iOSCharts.io"
+        pieChartView.chartDescription = d
+        pieChartView.centerText = "Pie Chart"
+        pieChartView.holeRadiusPercent = 0.4
+        pieChartView.transparentCircleColor = UIColor.clear
+        self.view.addSubview(pieChartView)
+    }
     
 }
