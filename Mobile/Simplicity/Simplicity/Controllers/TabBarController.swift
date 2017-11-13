639061a613759f1d73389e1efa52f056e13947cf
tanya-al <talrehan@ucla.edu>
2017-11-12 19:55:57 -0800

    changed tab bar item titles

--- Mobile/Simplicity/Simplicity/Controllers/TabBarController.swift
+++ Mobile/Simplicity/Simplicity/Controllers/TabBarController.swift
@@ -16,19 +16,23 @@ class TabBarController: UITabBarController {
         
         // Add View Controllers to Tab Bar
         let overviewViewController = OverviewViewController()
-        overviewViewController.tabBarItem = UITabBarItem(tabBarSystemItem: .downloads, tag: 0)
+        overviewViewController.tabBarItem = UITabBarItem(title: "Home", image: UIImage(named: "./../SupportFiles/Assets.xcassets/tabbaritems.imageset/home"), tag: 1)
         
         let transactionsViewController = TransactionsViewController()
         transactionsViewController.tabBarItem = UITabBarItem(tabBarSystemItem: .bookmarks, tag: 1)
+        transactionsViewController.tabBarItem = UITabBarItem(title: "Transactions", image: UIImage(named: "./../SupportFiles/Assets.xcassets/tabbaritems.imageset/transactions"), tag: 1)
         
         let addViewController = AddViewController()
         addViewController.tabBarItem = UITabBarItem(tabBarSystemItem: .bookmarks, tag: 2)
+        addViewController.tabBarItem = UITabBarItem(title: "Add", image: UIImage(named: "./../SupportFiles/Assets.xcassets/tabbaritems.imageset/add"), tag: 1)
         
         let receiptsViewController = ReceiptsViewController()
         receiptsViewController.tabBarItem = UITabBarItem(tabBarSystemItem: .bookmarks, tag: 3)
+        receiptsViewController.tabBarItem = UITabBarItem(title: "Receipts", image: UIImage(named: "./../SupportFiles/Assets.xcassets/tabbaritems.imageset/receipts"), tag: 1)
         
         let settingsViewController = SettingsViewController()
         settingsViewController.tabBarItem = UITabBarItem(tabBarSystemItem: .bookmarks, tag: 4)
+        settingsViewController.tabBarItem = UITabBarItem(title: "Settings", image: UIImage(named: "./../SupportFiles/Assets.xcassets/tabbaritems.imageset/settings"), tag: 1)
         
         let viewControllerList = [ overviewViewController,
                                    transactionsViewController,
@@ -37,6 +41,7 @@ class TabBarController: UITabBarController {
                                    settingsViewController ] as [Any]
         
         viewControllers = viewControllerList as? [UIViewController]
+        
     }
     
     override func didReceiveMemoryWarning() {
