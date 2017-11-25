271a0e6502c0622e073d59abda8188416a836b91
tanya-al <talrehan@ucla.edu>
2017-11-25 12:04:15 -0800

    got camera working and presenting in Photo VC

--- Mobile/Simplicity/Simplicity/Controllers/CameraViewController.swift
+++ Mobile/Simplicity/Simplicity/Controllers/CameraViewController.swift
@@ -10,13 +10,15 @@ import Foundation
 import UIKit
 import AVFoundation
 
-class CameraViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
+class CameraViewController: UIViewController, AVCapturePhotoCaptureDelegate {
     
     var captureSession: AVCaptureSession?
     var videoPreviewLayer: AVCaptureVideoPreviewLayer?
-    var takePhoto = false
+    var takePhotoVar = false
     var dataOutput : AVCaptureVideoDataOutput?
+    var stillCameraOutput : AVCapturePhotoOutput?
     var queue = DispatchQueue(label: "com.ItsLikeX4Y.Simplicity")
+    var imageView : UIImageView?
     
     enum errors : Error {
         case captureSessionMissing
@@ -41,30 +43,17 @@ class CameraViewController: UIViewController, AVCaptureVideoDataOutputSampleBuff
         videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
         self.view.layer.addSublayer(videoPreviewLayer!)
         videoPreviewLayer?.frame = self.view.layer.frame
-        captureSession?.startRunning()
         
         //capture image
-        dataOutput = AVCaptureVideoDataOutput()
-//        dataOutput?.setPreparedPhotoSettingsArray([AVCapturePhotoSettings(format: [AVVideoCodecKey : AVVideoCodecJPEG])], completionHandler: nil)
-        dataOutput?.videoSettings = [((kCVPixelBufferPixelFormatTypeKey as NSString) as String) : NSNumber(value: kCVPixelFormatType_420YpCbCr8BiPlanarFullRange as UInt32)]
-        dataOutput?.alwaysDiscardsLateVideoFrames = true
-        
-        if captureSession!.canAddOutput(dataOutput!) {
-            captureSession?.addOutput(dataOutput!)
+        stillCameraOutput = AVCapturePhotoOutput()
+        stillCameraOutput?.setPreparedPhotoSettingsArray([AVCapturePhotoSettings(format: [AVVideoCodecKey : AVVideoCodecJPEG])], completionHandler: nil)
+
+        if self.captureSession!.canAddOutput(self.stillCameraOutput!) {
+            self.captureSession?.addOutput(self.stillCameraOutput!)
         }
         captureSession?.commitConfiguration()
+        captureSession?.startRunning()
 
-        dataOutput?.setSampleBufferDelegate(self, queue: queue)
-    }
-    
-    @objc func displayPreview(on view: UIView) throws {
-       let photoVC = PhotoViewController(capturedImage: UIImageView(frame: (videoPreviewLayer?.frame)!))
-        DispatchQueue.main.async {
-            self.present(photoVC, animated: true, completion: nil)
-        }
-        
-//        view.layer.insertSublayer(self.videoPreviewLayer!, at: 0)
-//        self.videoPreviewLayer?.frame = view.frame
     }
     
     func displayButton() {
@@ -78,47 +67,50 @@ class CameraViewController: UIViewController, AVCaptureVideoDataOutputSampleBuff
         captureButton.clipsToBounds = true
         captureButton.backgroundColor = .red
         captureButton.center = CGPoint(x: horizontalCenter, y: verticalBottom-40)
-        captureButton.addTarget(self, action: #selector(displayPreview(on:)), for: .touchUpInside)
+        captureButton.addTarget(self, action: #selector(takePhoto(_:)), for: .touchUpInside)
         self.view.insertSubview(captureButton, at: 0)
     }
     
-    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
-        if takePhoto {
-            takePhoto = false
-            //getImagefromsmaplebuffer
-            if let image = self.getImageFromSampleBuffer(buffer: sampleBuffer) {
-                let photoVC = PhotoViewController(capturedImage: UIImageView(image: image))
-                
+    @objc func takePhoto(_ sender : Any) {
+        capturePhoto()
+    }
+    
+    func capturePhoto() {
+        
+        let settings = AVCapturePhotoSettings()
+        let previewPixelType = settings.availablePreviewPhotoPixelFormatTypes.first!
+        let previewFormat = [kCVPixelBufferPixelFormatTypeKey as String: previewPixelType,
+                             kCVPixelBufferWidthKey as String: 160,
+                             kCVPixelBufferHeightKey as String: 160]
+        settings.previewPhotoFormat = previewFormat
+        self.stillCameraOutput?.capturePhoto(with: settings, delegate: self)
+        
+    }
+    
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photoSampleBuffer: CMSampleBuffer?, previewPhoto previewPhotoSampleBuffer: CMSampleBuffer?, resolvedSettings: AVCaptureResolvedPhotoSettings, bracketSettings: AVCaptureBracketedStillImageSettings?, error: Error?) {
+
+        if let error = error {
+            print(error.localizedDescription)
+        }
+        
+        if let sampleBuffer = photoSampleBuffer, let previewBuffer = previewPhotoSampleBuffer, let dataImage = AVCapturePhotoOutput.jpegPhotoDataRepresentation(forJPEGSampleBuffer: sampleBuffer, previewPhotoSampleBuffer: previewBuffer) {
+            
+            if let image = UIImage(data: dataImage) {
+                print(image.size)
+                let imageView = UIImageView(image: image)
+                let photoVC = PhotoViewController(capturedImage: imageView)
+
                 DispatchQueue.main.async {
                     self.present(photoVC, animated: true, completion: nil)
                 }
             }
         }
-    }
-    
-    func getImageFromSampleBuffer (buffer : CMSampleBuffer) -> UIImage? {
-        if let pixelBuffer = CMSampleBufferGetImageBuffer(buffer) {
-            let ciImage = CIImage(cvPixelBuffer: pixelBuffer)
-            let context = CIContext()
-            
-            let imageRect = CGRect(x: 0, y: 0, width: CVPixelBufferGetWidth(pixelBuffer), height: CVPixelBufferGetHeight(pixelBuffer))
-            
-            if let image = context.createCGImage(ciImage, from: imageRect) {
-                return UIImage(cgImage: image, scale: UIScreen.main.scale, orientation: .right)
-            }
-        }
-        return nil
-    }
-    
-    @IBAction func takePhoto(_ sender  : Any) {
-        takePhoto = true
-        captureOutput(dataOutput!, didOutput: dataOutput?.sampleBufferDelegate as! CMSampleBuffer, from: (videoPreviewLayer?.connection)!)
+        
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
     
 }
