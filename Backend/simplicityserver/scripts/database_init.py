56c0ddbfe1152f4a4d242cebc8bad248faa000c5
Lake Mossman <lakemossman@Lakes-MacBook-Pro.local>
2017-12-06 18:52:48 -0800

    add dummy image for each transaction

--- Backend/simplicityserver/scripts/database_init.py
+++ Backend/simplicityserver/scripts/database_init.py
@@ -73,146 +73,147 @@ if __name__ == '__main__':
 
 	#====================== overview dummy data ======================#
 		# week 0 #
+	b64 = encode_b64(Image.open(os.path.join(RECEIPTSPATH, "1002-3.jpg")))
 	cur.execute('''	INSERT INTO transactions (transaction_id, user_id, category, amount, date, image)
-					VALUES (?, 42069, "Restaurant", 50.00, "2017-12-05 19:29:00", "image_b64")''', (transaction_id,))
+					VALUES (?, 42069, "Restaurant", 50.00, "2017-12-05 19:29:00", ?)''', (transaction_id, b64))
 	transaction_id += 1
 
 	cur.execute('''	INSERT INTO transactions (transaction_id, user_id, category, amount, date, image)
-					VALUES (?, 42069, "Transportation", 15.00, "2017-12-05 19:30:00", "image_b64")''', (transaction_id,))
+					VALUES (?, 42069, "Transportation", 15.00, "2017-12-05 19:30:00", ?)''', (transaction_id, b64))
 	transaction_id += 1
 
 	cur.execute('''	INSERT INTO transactions (transaction_id, user_id, category, amount, date, image)
-					VALUES (?, 42069, "Textbook", 70.00, "2017-12-05 19:31:00", "image_b64")''', (transaction_id,))
+					VALUES (?, 42069, "Textbook", 70.00, "2017-12-05 19:31:00", ?)''', (transaction_id, b64))
 	transaction_id += 1
 
 	cur.execute('''	INSERT INTO transactions (transaction_id, user_id, category, amount, date, image)
-					VALUES (?, 42069, "Grocery", 42.00, "2017-12-05 19:32:00", "image_b64")''', (transaction_id,))
+					VALUES (?, 42069, "Grocery", 42.00, "2017-12-05 19:32:00", ?)''', (transaction_id, b64))
 	transaction_id += 1
 
 	cur.execute('''	INSERT INTO transactions (transaction_id, user_id, category, amount, date, image)
-					VALUES (?, 42069, "Other", 19.00, "2017-12-05 19:33:00", "image_b64")''', (transaction_id,))
+					VALUES (?, 42069, "Other", 19.00, "2017-12-05 19:33:00", ?)''', (transaction_id, b64))
 	transaction_id += 1
 
 		# week 1 #
 	cur.execute('''	INSERT INTO transactions (transaction_id, user_id, category, amount, date, image)
-					VALUES (?, 42069, "Restaurant", 20.00, "2017-11-28 19:29:00", "image_b64")''', (transaction_id,))
+					VALUES (?, 42069, "Restaurant", 20.00, "2017-11-28 19:29:00", ?)''', (transaction_id, b64))
 	transaction_id += 1
 
 	cur.execute('''	INSERT INTO transactions (transaction_id, user_id, category, amount, date, image)
-					VALUES (?, 42069, "Transportation", 52.00, "2017-11-28 19:30:00", "image_b64")''', (transaction_id,))
+					VALUES (?, 42069, "Transportation", 52.00, "2017-11-28 19:30:00", ?)''', (transaction_id, b64))
 	transaction_id += 1
 
 	cur.execute('''	INSERT INTO transactions (transaction_id, user_id, category, amount, date, image)
-					VALUES (?, 42069, "Textbook", 30.00, "2017-11-28 19:31:00", "image_b64")''', (transaction_id,))
+					VALUES (?, 42069, "Textbook", 30.00, "2017-11-28 19:31:00", ?)''', (transaction_id, b64))
 	transaction_id += 1
 
 	cur.execute('''	INSERT INTO transactions (transaction_id, user_id, category, amount, date, image)
-					VALUES (?, 42069, "Grocery", 37.00, "2017-11-28 19:32:00", "image_b64")''', (transaction_id,))
+					VALUES (?, 42069, "Grocery", 37.00, "2017-11-28 19:32:00", ?)''', (transaction_id, b64))
 	transaction_id += 1
 
 	cur.execute('''	INSERT INTO transactions (transaction_id, user_id, category, amount, date, image)
-					VALUES (?, 42069, "Other", 23.00, "2017-11-28 19:33:00", "image_b64")''', (transaction_id,))
+					VALUES (?, 42069, "Other", 23.00, "2017-11-28 19:33:00", ?)''', (transaction_id, b64))
 	transaction_id += 1
 
 		# week 2 #
 	cur.execute('''	INSERT INTO transactions (transaction_id, user_id, category, amount, date, image)
-					VALUES (?, 42069, "Restaurant", 40.00, "2017-11-21 19:29:00", "image_b64")''', (transaction_id,))
+					VALUES (?, 42069, "Restaurant", 40.00, "2017-11-21 19:29:00", ?)''', (transaction_id, b64))
 	transaction_id += 1
 
 	cur.execute('''	INSERT INTO transactions (transaction_id, user_id, category, amount, date, image)
-					VALUES (?, 42069, "Transportation", 19.00, "2017-11-21 19:30:00", "image_b64")''', (transaction_id,))
+					VALUES (?, 42069, "Transportation", 19.00, "2017-11-21 19:30:00", ?)''', (transaction_id, b64))
 	transaction_id += 1
 
 	cur.execute('''	INSERT INTO transactions (transaction_id, user_id, category, amount, date, image)
-					VALUES (?, 42069, "Textbook", 10.00, "2017-11-21 19:31:00", "image_b64")''', (transaction_id,))
+					VALUES (?, 42069, "Textbook", 10.00, "2017-11-21 19:31:00", ?)''', (transaction_id, b64))
 	transaction_id += 1
 
 	cur.execute('''	INSERT INTO transactions (transaction_id, user_id, category, amount, date, image)
-					VALUES (?, 42069, "Grocery", 55.00, "2017-11-21 19:32:00", "image_b64")''', (transaction_id,))
+					VALUES (?, 42069, "Grocery", 55.00, "2017-11-21 19:32:00", ?)''', (transaction_id, b64))
 	transaction_id += 1
 
 	cur.execute('''	INSERT INTO transactions (transaction_id, user_id, category, amount, date, image)
-					VALUES (?, 42069, "Other", 43.00, "2017-11-21 19:33:00", "image_b64")''', (transaction_id,))
+					VALUES (?, 42069, "Other", 43.00, "2017-11-21 19:33:00", ?)''', (transaction_id, b64))
 	transaction_id += 1
 
 		# week 3 #
 	cur.execute('''	INSERT INTO transactions (transaction_id, user_id, category, amount, date, image)
-					VALUES (?, 42069, "Restaurant", 60.00, "2017-11-14 19:29:00", "image_b64")''', (transaction_id,))
+					VALUES (?, 42069, "Restaurant", 60.00, "2017-11-14 19:29:00", ?)''', (transaction_id, b64))
 	transaction_id += 1
 
 	cur.execute('''	INSERT INTO transactions (transaction_id, user_id, category, amount, date, image)
-					VALUES (?, 42069, "Transportation", 44.00, "2017-11-14 19:30:00", "image_b64")''', (transaction_id,))
+					VALUES (?, 42069, "Transportation", 44.00, "2017-11-14 19:30:00", ?)''', (transaction_id, b64))
 	transaction_id += 1
 
 	cur.execute('''	INSERT INTO transactions (transaction_id, user_id, category, amount, date, image)
-					VALUES (?, 42069, "Textbook", 10.00, "2017-11-14 19:31:00", "image_b64")''', (transaction_id,))
+					VALUES (?, 42069, "Textbook", 10.00, "2017-11-14 19:31:00", ?)''', (transaction_id, b64))
 	transaction_id += 1
 
 	cur.execute('''	INSERT INTO transactions (transaction_id, user_id, category, amount, date, image)
-					VALUES (?, 42069, "Grocery", 39.00, "2017-11-14 19:32:00", "image_b64")''', (transaction_id,))
+					VALUES (?, 42069, "Grocery", 39.00, "2017-11-14 19:32:00", ?)''', (transaction_id, b64))
 	transaction_id += 1
 
 	cur.execute('''	INSERT INTO transactions (transaction_id, user_id, category, amount, date, image)
-					VALUES (?, 42069, "Other", 26.00, "2017-11-14 19:33:00", "image_b64")''', (transaction_id,))
+					VALUES (?, 42069, "Other", 26.00, "2017-11-14 19:33:00", ?)''', (transaction_id, b64))
 	transaction_id += 1
 
 		# week 4 #
 	cur.execute('''	INSERT INTO transactions (transaction_id, user_id, category, amount, date, image)
-					VALUES (?, 42069, "Restaurant", 45.00, "2017-11-07 19:29:00", "image_b64")''', (transaction_id,))
+					VALUES (?, 42069, "Restaurant", 45.00, "2017-11-07 19:29:00", ?)''', (transaction_id, b64))
 	transaction_id += 1
 
 	cur.execute('''	INSERT INTO transactions (transaction_id, user_id, category, amount, date, image)
-					VALUES (?, 42069, "Transportation", 33.00, "2017-11-07 19:30:00", "image_b64")''', (transaction_id,))
+					VALUES (?, 42069, "Transportation", 33.00, "2017-11-07 19:30:00", ?)''', (transaction_id, b64))
 	transaction_id += 1
 
 	cur.execute('''	INSERT INTO transactions (transaction_id, user_id, category, amount, date, image)
-					VALUES (?, 42069, "Textbook", 10.00, "2017-11-07 19:31:00", "image_b64")''', (transaction_id,))
+					VALUES (?, 42069, "Textbook", 10.00, "2017-11-07 19:31:00", ?)''', (transaction_id, b64))
 	transaction_id += 1
 
 	cur.execute('''	INSERT INTO transactions (transaction_id, user_id, category, amount, date, image)
-					VALUES (?, 42069, "Grocery", 44.00, "2017-11-07 19:32:00", "image_b64")''', (transaction_id,))
+					VALUES (?, 42069, "Grocery", 44.00, "2017-11-07 19:32:00", ?)''', (transaction_id, b64))
 	transaction_id += 1
 
 	cur.execute('''	INSERT INTO transactions (transaction_id, user_id, category, amount, date, image)
-					VALUES (?, 42069, "Other", 33.00, "2017-11-07 19:33:00", "image_b64")''', (transaction_id,))
+					VALUES (?, 42069, "Other", 33.00, "2017-11-07 19:33:00", ?)''', (transaction_id, b64))
 	transaction_id += 1
 	#==================== end overview dummy data ====================#
 
 	#===================== breakdown dummy data ======================#
 	cur.execute('''	INSERT INTO transactions (transaction_id, user_id, category, amount, date, image)
-					VALUES (?, 99, "restaurant", 10.00, "2017-11-24 19:29:00", "image_b64")''', (transaction_id,))
+					VALUES (?, 99, "restaurant", 10.00, "2017-11-24 19:29:00", ?)''', (transaction_id, b64))
 	transaction_id += 1
 
 	cur.execute('''	INSERT INTO transactions (transaction_id, user_id, category, amount, date, image)
-					VALUES (?, 99, "restaurant", 15.00, "2017-11-23 19:29:00", "image_b64")''', (transaction_id,))
+					VALUES (?, 99, "restaurant", 15.00, "2017-11-23 19:29:00", ?)''', (transaction_id, b64))
 	transaction_id += 1
 
 	cur.execute('''	INSERT INTO transactions (transaction_id, user_id, category, amount, date, image)
-					VALUES (?, 99, "grocery", 5.00, "2017-11-23 19:29:00", "image_b64")''', (transaction_id,))
+					VALUES (?, 99, "grocery", 5.00, "2017-11-23 19:29:00", ?)''', (transaction_id, b64))
 	transaction_id += 1
 
 	cur.execute('''	INSERT INTO transactions (transaction_id, user_id, category, amount, date, image)
-					VALUES (?, 99, "other", 5.00, "2017-11-23 19:29:00", "image_b64")''', (transaction_id,))
+					VALUES (?, 99, "other", 5.00, "2017-11-23 19:29:00", ?)''', (transaction_id, b64))
 	transaction_id += 1
 
 	cur.execute('''	INSERT INTO transactions (transaction_id, user_id, category, amount, date, image)
-					VALUES (?, 99, "grocery", 30.00, "2017-11-15 19:30:00", "image_b64")''', (transaction_id,))
+					VALUES (?, 99, "grocery", 30.00, "2017-11-15 19:30:00", ?)''', (transaction_id, b64))
 	transaction_id += 1
 
 	cur.execute('''	INSERT INTO transactions (transaction_id, user_id, category, amount, date, image)
-					VALUES (?, 99, "other", 20.00, "2017-11-16 19:31:00", "image_b64")''', (transaction_id,))
+					VALUES (?, 99, "other", 20.00, "2017-11-16 19:31:00", ?)''', (transaction_id, b64))
 	transaction_id += 1
 
 	cur.execute('''	INSERT INTO transactions (transaction_id, user_id, category, amount, date, image)
-					VALUES (?, 99, "restaurant", 10.00, "2017-11-08 19:32:00", "image_b64")''', (transaction_id,))
+					VALUES (?, 99, "restaurant", 10.00, "2017-11-08 19:32:00", ?)''', (transaction_id, b64))
 	transaction_id += 1
 
 	cur.execute('''	INSERT INTO transactions (transaction_id, user_id, category, amount, date, image)
-					VALUES (?, 99, "other", 20.00, "2017-11-09 19:33:00", "image_b64")''', (transaction_id,))
+					VALUES (?, 99, "other", 20.00, "2017-11-09 19:33:00", ?)''', (transaction_id, b64))
 	transaction_id += 1
 
 	cur.execute('''	INSERT INTO transactions (transaction_id, user_id, category, amount, date, image)
-					VALUES (?, 99, "other", 25.00, "2017-11-08 19:33:00", "image_b64")''', (transaction_id,))
+					VALUES (?, 99, "other", 25.00, "2017-11-08 19:33:00", ?)''', (transaction_id, b64))
 	transaction_id += 1
 	#=================== end breakdown dummy data ====================#
 
