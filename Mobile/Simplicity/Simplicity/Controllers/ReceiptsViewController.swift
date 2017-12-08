a4f894d1b14486b1b49a5a5a596f27b3f61ec50d
Anthony Lai <yukuan.anthony.lai@gmail.com>
2017-12-07 16:24:55 -0800

    Setup CollectionViewController

--- Mobile/Simplicity/Simplicity/Controllers/ReceiptsViewController.swift
+++ Mobile/Simplicity/Simplicity/Controllers/ReceiptsViewController.swift
@@ -8,7 +8,13 @@
 
 import UIKit
 
-class ReceiptsViewController: UIViewController {
+class ReceiptsViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    
+    let cellId = "cellId"
+    
+    // MARK: Properties
+    var mainNavigationBar: MainNavigationBar?
+    //var collectionView: UICollectionView?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,8 +23,72 @@ class ReceiptsViewController: UIViewController {
         view.backgroundColor = UIColor.white
         self.title = "Receipts"
         
-        let mainNavigationBar = MainNavigationBar(frame: view.frame, title: "Receipts")
-        view.addSubview(mainNavigationBar)
+        mainNavigationBar = MainNavigationBar(frame: view.frame, title: "Receipts")
+        view.addSubview(mainNavigationBar!)
+        
+
+        setupCollectionView()
+        
+        //setupCollectionView()
+        
+//        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+//        layout.sectionInset = UIEdgeInsets(top: 20, left: 10, bottom: 10, right: 10)
+//        layout.itemSize = CGSize(width: 60, height: 60)
+        
+        //let myCollectionView:UICollectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
+//        myCollectionView.dataSource = self
+//        myCollectionView.delegate = self
+        //myCollectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "MyCell")
+//        myCollectionView.backgroundColor = UIColor.blue
+//        self.view.addSubview(myCollectionView)
+    }
+    
+    func setupCollectionView() {
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 20, left: 10, bottom: 10, right: 10)
+        layout.itemSize = CGSize(width: 60, height: 60)
+        
+        collectionView!.collectionViewLayout = layout
+        
+//        collectionView = UICollectionView(frame: CGRect(x: 0,
+//                                                        y: mainNavigationBar!.frame.height,
+//                                                        width: self.view.frame.width,
+//                                                        height: self.view.frame.height - mainNavigationBar!.frame.height),
+//                                          collectionViewLayout: layout)
+        collectionView!.frame = CGRect(x: 0,
+                                       y: mainNavigationBar!.frame.height,
+                                   width: self.view.frame.width,
+                                  height: self.view.frame.height - mainNavigationBar!.frame.height)
+        
+        collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+        collectionView!.backgroundColor = UIColor.blue
+        
+        //collectionView!.dataSource = self
+        //collectionView!.delegate = self
+        
+        //self.view.addSubview(collectionView!)
+    }
+    
+    // MARK: UICollectionViewDataSource
+    
+//    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        return 20
+//    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        print("numbers?")
+        return 100
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        cell.backgroundColor = UIColor.green
+        return cell
+    }
+
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath)
+    {
+        print("User tapped on item \(indexPath.row)")
     }
     
     override func didReceiveMemoryWarning() {
