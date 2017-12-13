11c4de79681364f2d4b413ef737e3b42b3918194
tanya-al <talrehan@ucla.edu>
2017-12-13 14:55:09 -0800

    started fb login setup

---
+++ Mobile/Simplicity/Simplicity/Controllers/LoginViewController.swift
@@ -0,0 +1,14 @@
+//
+//  LoginViewController.swift
+//  Simplicity
+//
+//  Created by Tanya A on 12/13/17.
+//  Copyright © 2017 LikeX4Y. All rights reserved.
+//
+
+import Foundation
+import FacebookLogin
+
+class LoginViewController : UIViewController {
+    
+}
