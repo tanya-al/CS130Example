51a6631575a20498f4ddd0a295dd7183be1ffda2
tanya-al <talrehan@ucla.edu>
2017-12-13 15:33:15 -0800

    adding fb login stuff

--- Mobile/Simplicity/Simplicity/Controllers/LoginViewController.swift
+++ Mobile/Simplicity/Simplicity/Controllers/LoginViewController.swift
@@ -8,7 +8,53 @@
 
 import Foundation
 import FacebookLogin
+import FBSDKLoginKit
 
 class LoginViewController : UIViewController {
+    var dict : [String : AnyObject]!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //creating button
+        let loginButton = LoginButton(readPermissions: [ .publicProfile ])
+        loginButton.center = view.center
+        
+        //adding it to view
+        view.addSubview(loginButton)
+        
+        //if the user is already logged in
+        if let accessToken = FBSDKAccessToken.current(){
+            getFBUserData()
+        }
+        
+    }
+    
+    //when login button clicked
+    @objc func loginButtonClicked() {
+        let loginManager = LoginManager()
+        loginManager.logIn([ .publicProfile ], viewController: self) { loginResult in
+            switch loginResult {
+            case .failed(let error):
+                print(error)
+            case .cancelled:
+                print("User cancelled login.")
+            case .success(let grantedPermissions, let declinedPermissions, let accessToken):
+                self.getFBUserData()
+            }
+        }
+    }
+    
+    //function is fetching the user data
+    func getFBUserData(){
+        if((FBSDKAccessToken.current()) != nil){
+            FBSDKGraphRequest(graphPath: "me", parameters: ["fields": "id, name, picture.type(large), email"]).start(completionHandler: { (connection, result, error) -> Void in
+                if (error == nil){
+                    self.dict = result as! [String : AnyObject]
+                    print(result!)
+                    print(self.dict)
+                }
+            })
+        }
+    }
 }
