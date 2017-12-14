aa8617b16cc25d49cd6b08cbe01b8f5cc6ee2b08
tanya-al <talrehan@ucla.edu>
2017-12-13 16:38:03 -0800

    logged in successfully

--- Mobile/Simplicity/Simplicity/Controllers/LoginViewController.swift
+++ Mobile/Simplicity/Simplicity/Controllers/LoginViewController.swift
@@ -13,6 +13,25 @@ import FBSDKLoginKit
 class LoginViewController : UIViewController {
     var dict : [String : AnyObject]!
     
+    override func viewDidAppear(_ animated: Bool) {
+        if let accessToken = FBSDKAccessToken.current(){
+            print(accessToken)
+            var userData = getFBUserData()
+            
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let initialViewController = storyboard.instantiateViewController(withIdentifier: "TabBarController")
+            UIApplication.shared.keyWindow?.rootViewController = initialViewController
+            UIApplication.shared.keyWindow?.makeKeyAndVisible()
+            
+            let statusBar: UIView = UIApplication.shared.value(forKey: "statusBar") as! UIView
+            if statusBar.responds(to:#selector(setter: UIView.backgroundColor)) {
+                statusBar.backgroundColor = UIColor.clear
+            }
+            UIApplication.shared.statusBarStyle = .lightContent
+            print("did the thing")
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
@@ -28,7 +47,6 @@ class LoginViewController : UIViewController {
         if let accessToken = FBSDKAccessToken.current(){
             getFBUserData()
         }
-        
     }
     
     override func didReceiveMemoryWarning() {
@@ -46,21 +64,25 @@ class LoginViewController : UIViewController {
             case .cancelled:
                 print("User cancelled login.")
             case .success(let grantedPermissions, let declinedPermissions, let accessToken):
+                print("logged in")
                 self.getFBUserData()
             }
         }
     }
 
     //function is fetching the user data
-    func getFBUserData(){
+    func getFBUserData() -> [String: Any] {
+        var userData : [String: Any] = [:]
         if((FBSDKAccessToken.current()) != nil){
             FBSDKGraphRequest(graphPath: "me", parameters: ["fields": "id, name, picture.type(large), email"]).start(completionHandler: { (connection, result, error) -> Void in
                 if (error == nil){
                     self.dict = result as! [String : AnyObject]
                     print(result!)
                     print(self.dict)
+                    userData = self.dict
                 }
             })
         }
+        return userData
     }
 }
