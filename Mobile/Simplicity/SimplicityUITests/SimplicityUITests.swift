0bf39ac9605c8a8b374a2b89c2fe427ab1445732
tanya-al <talrehan@ucla.edu>
2017-12-14 17:18:29 -0800

    finish tests

--- Mobile/Simplicity/SimplicityUITests/SimplicityUITests.swift
+++ Mobile/Simplicity/SimplicityUITests/SimplicityUITests.swift
@@ -75,8 +75,71 @@ class SimplicityUITests: XCTestCase {
         
         sleep(1)
         
-        //hit setting button
-        tabBarsQuery.buttons["Settings"].tap()
+        tabBarsQuery.buttons["Add"].tap()
+        sleep(2);
+        app.buttons["Take Photo"].tap()
+        sleep(2);
+        app.children(matching: .window).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element.children(matching: .button).element.tap()
+        sleep(2);
+        let enterReceiptDescriptionHereTextField = app.textFields["Enter receipt description here"]
+        enterReceiptDescriptionHereTextField.tap()
+        sleep(2);
+        enterReceiptDescriptionHereTextField.typeText("F")
+        sleep(2);
+        
+        let app2 = app
+        sleep(2);
+        app2/*@START_MENU_TOKEN@*/.keys["o"]/*[[".keyboards.keys[\"o\"]",".keys[\"o\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        sleep(2);
+        enterReceiptDescriptionHereTextField.typeText("ood")
+        sleep(2);
+        app.textFields["Enter category of expense here"].tap()
+        sleep(2);
+        app2/*@START_MENU_TOKEN@*/.pickerWheels["Restaurant"].tap()/*[[".pickers.pickerWheels[\"Restaurant\"]",".tap()",".press(forDuration: 1.1);",".pickerWheels[\"Restaurant\"]"],[[[-1,3,1],[-1,0,1]],[[-1,2],[-1,1]]],[0,1]]@END_MENU_TOKEN@*/
+        sleep(20);
+        app.buttons["Save Image"].tap()
+        sleep(2);
+        
+        app.alerts["Confirm Transaction"].buttons["No"].tap()
+        sleep(2);
+
+        let moreKey = app2/*@START_MENU_TOKEN@*/.keys["more"]/*[[".keyboards",".keys[\"more, numbers\"]",".keys[\"more\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/
+        sleep(2);
+        moreKey.tap()
+        sleep(2);
+        app2/*@START_MENU_TOKEN@*/.keys["3"]/*[[".keyboards.keys[\"3\"]",".keys[\"3\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        sleep(2);
+
+        let updateTransactionAlert = app.alerts["Update Transaction"]
+        sleep(2);
+        let enterAmountTextField = updateTransactionAlert.collectionViews.textFields["Enter amount"]
+        sleep(2);
+        enterAmountTextField.typeText("3")
+        sleep(2);
+        app2/*@START_MENU_TOKEN@*/.keys["."]/*[[".keyboards.keys[\".\"]",".keys[\".\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        sleep(2);
+        enterAmountTextField.typeText(".")
+        sleep(2);
+        app2/*@START_MENU_TOKEN@*/.keys["9"]/*[[".keyboards.keys[\"9\"]",".keys[\"9\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        sleep(2);
+        enterAmountTextField.typeText("9")
+        sleep(2);
+        app2/*@START_MENU_TOKEN@*/.keys["0"]/*[[".keyboards.keys[\"0\"]",".keys[\"0\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        sleep(2);
+        enterAmountTextField.typeText("0")
+        sleep(2);
+        updateTransactionAlert.buttons["Submit"].tap()
+        
+        sleep(40);
+        let receiptsButton = tabBarsQuery.buttons["Receipts"]
+        receiptsButton.tap()
+        sleep(2);
+        app.collectionViews.children(matching: .cell).element(boundBy: 0).children(matching: .other).element.tap()
+        sleep(5);
+        //app.statusBars.otherElements["2 of 5 bars, signal strength"].tap()
+        app.buttons["Back"].tap()
+        sleep(2);
+        tabBarsQuery.buttons["Transactions"].tap()
     }
     
 }
